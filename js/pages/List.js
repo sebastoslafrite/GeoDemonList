4e74189384d254c3f114a6c12f030835eed9b9c3
jak9936 <jack88dl@gmail.com>
2024-06-22 13:28:17 -0400

    Update List.js
    updated rules

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,7 +100,7 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without using hacks. For now, this also includes the usage of Click Between Frames. (however, FPS bypass is allowed, up to 360fps pre 2.2 and there is no fps cap for 2.2. Physics bypass is not allowed)
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
@@ -109,7 +109,7 @@ export default {
                         Have either source audio or clicks/taps in the video. Edited audio only does not count
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        High end insanes (check google sheet) and beyond will need video proof and session footage. The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. 
                     </p>
                     <p>
                         The recording must also show the player hit the endwall, or the completion will be invalidated.
@@ -118,7 +118,7 @@ export default {
                         Do not use secret routes or bug routes
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        More info can be found in the discord and google sheet
                     </p>
                     
                 </div>
